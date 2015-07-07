b4759516a67a141d56b3d0883068f42a6bfbd541
Max Käufer <am-kay@web.de>
2015-07-07 18:33:55 +0200

    Remove unnecessary public modifier in DetachedHeadStrategy

--- src/main/groovy/com/github/amkay/gradle/gitflow/strategy/DetachedHeadStrategy.groovy
+++ src/main/groovy/com/github/amkay/gradle/gitflow/strategy/DetachedHeadStrategy.groovy
@@ -28,7 +28,7 @@ import static com.github.amkay.gradle.gitflow.version.VersionType.DETACHED_HEAD
  *
  * @author Max Käufer
  */
-public class DetachedHeadStrategy extends AbstractStrategy {
+class DetachedHeadStrategy extends AbstractStrategy {
 
     @Override
     protected VersionWithType doInfer(final Grgit grgit, final GitflowPluginExtension ext) {

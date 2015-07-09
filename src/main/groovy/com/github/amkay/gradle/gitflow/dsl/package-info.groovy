2a09fa7469998079bfa996d7bfc294bfcf863d59
Max Käufer <am-kay@web.de>
2015-07-09 03:01:55 +0200

    Make first sentence in dsl package fit in overview

--- src/main/groovy/com/github/amkay/gradle/gitflow/dsl/package-info.groovy
+++ src/main/groovy/com/github/amkay/gradle/gitflow/dsl/package-info.groovy
@@ -1,6 +1,6 @@
 /**
- * Provides all classes for the well-known <em>keyword-based configuration</em> (a.k.a. <em>DSL</em>) that all
- * default <em>Gradle</em> plugins provide.
+ * Provides all classes for the well-known <em>keyword-based configuration</em> or <em>DSL</em> that all default
+ * <em>Gradle</em> plugins provide.
  *
  * <p>
  *     The entry point to this configuration is {@link GitflowPluginExtension}.
